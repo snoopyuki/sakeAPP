bc9e937eff4b2e64a2de50354c6a487d0e14c91c
gityuuhub <93773459+gityuuhub@users.noreply.github.com>
2021-12-30 09:54:55 +0900

    CD構築前

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import Box from "@mui/material/Box";
 
-import Menu from "./components/menu";
+import { Menu } from "./components/menu";
 import { Header } from "./components/header";
 import "./styles.css";
 
 export const App = () => {
-  //  const [prefecture, setPrefecture] = useState(["11", "aa"]);
+  const [prefecture, setPrefecture] = useState("11");
 
   return (
     <>
@@ -14,13 +14,12 @@ export const App = () => {
         <Header>さけのわあぷり</Header>
       </Box>
       <Box component="span" display="flex" m={1}>
-        <div>
-          <h3>メニュー</h3>
-          <Menu />
-        </div>
+        <Menu setPrefecture={setPrefecture} prefecture={prefecture} />
       </Box>
       <Box component="span" display="flex" m={1}>
-        <div>{/* <p>{prefecture[0]}</p> */}</div>
+        <div>
+          <p>{prefecture}</p>
+        </div>
       </Box>
     </>
   );
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -2,66 +2,74 @@ import React, { useState } from "react";
 import { Button, Drawer } from "@material-ui/core";
 import Box from "@mui/material/Box";
 
-const onClickNextPage = () => {
-  // 特に処理はない
-};
-const onClickDoAPI = () => {
-  alert("API実行時はCORS問題を解決すること。");
-
-  fetch("https://muro.sakenowa.com/sakenowa-data/api/areas", {
-    mode: "cors"
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      //const [copylight, areas] = { ...data };
-      console.log(data.areas[0].name);
-    })
-    .catch((error) => {
-      console.log("失敗しました");
-    });
-};
 const menuStyle = {
   background: "#AAA"
 };
-export default function Menu() {
+
+export const Menu = (props) => {
   const [open, setopen] = useState(false);
   const toggleOpen = () => {
     setopen(!open);
   };
 
+  const onClickNextPage = () => {
+    // 特に処理はない
+  };
+  const onClickDoAPI = () => {
+    fetch("https://muro.sakenowa.com/sakenowa-data/api/areas", {
+      mode: "cors"
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        // API実行結果をpropsに格納
+        console.log(data.areas[0].name);
+        // ToDO配列の中身をループで回す。
+        props.setPrefecture(data.areas[0].name);
+      })
+      .catch((error) => {
+        alert("API実行時はCORS問題を解決すること。");
+        console.log("失敗しました");
+      });
+  };
+
   return (
     <>
-      <Button variant="outlined" onClick={toggleOpen}>
-        open>>
-      </Button>
-      <Drawer anchor="left" open={open} onClose={toggleOpen}>
-        <h4>どろわーメニュー</h4>
-        <Box m={1} mt={10} style={menuStyle}>
-          <a
-            href="https://muro.sakenowa.com/sakenowa-data/api/areas"
-            target="_blank"
-          >
+      <div>
+        <h3>メニュー</h3>
+        <Button variant="outlined" onClick={toggleOpen}>
+          open>>
+        </Button>
+        <Drawer anchor="left" open={open} onClose={toggleOpen}>
+          <h4>どろわーメニュー</h4>
+          <Box m={1} mt={10} style={menuStyle}>
+            <a
+              href="https://muro.sakenowa.com/sakenowa-data/api/areas"
+              target="_blank"
+            >
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => onClickNextPage()}
+              >
+                産地一覧取得WEB遷移
+              </Button>
+            </a>
+            <br />
+            <br />
             <Button
               variant="contained"
               color="primary"
-              onClick={() => onClickNextPage()}
+              onClick={() => {
+                onClickDoAPI();
+              }}
             >
-              産地一覧取得WEB遷移
+              産地一覧取得API実行
             </Button>
-          </a>
-          <br />
-          <br />
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => onClickDoAPI()}
-          >
-            産地一覧取得API実行
-          </Button>
-        </Box>
-      </Drawer>
+          </Box>
+        </Drawer>
+      </div>
     </>
   );
-}
+};
